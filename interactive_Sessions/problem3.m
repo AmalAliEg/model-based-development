903a3914cccfd17c92b4c5744427f6d00ca9405d
amal_ali <aml_ali_hassan@outlook.com>
2024-09-21 16:42:06 +0300

    deleted:    interactive_Sessions/problem2.asv 	new file:   interactive_Sessions/problem3.m

---
+++ interactive_Sessions/problem3.m
@@ -0,0 +1,17 @@
+
+A1=1:7;      %row_vector
+
+B=repmat(A1,7,1);      %repeated 7 times as row and 1 time as column
+
+%loop over the matrix and zero the element he i or j or both are 4.
+disp(B);
+
+for i =1:7
+    for j=1:7
+        if i==4||j==4
+            B(i,j)=0;
+        end 
+    end 
+end
+
+disp(B);
